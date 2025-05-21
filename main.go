ec490d57487f44c29477d5d6c69b449f50acb02b
Simon Kwong <skwong5@ucsc.edu>
2025-05-21 15:12:01 -0700

    update

--- main.go
+++ main.go
@@ -4,18 +4,43 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"syscall"
 )
+
+const BUFFER_SIZE = 4096
+
+func read_bytes(buf *[]byte, infile string) {
+	fmt.Printf("Reading %s\n", infile)
+	fd, err := syscall.Open(infile, syscall.O_RDONLY, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer syscall.Close(fd)
+
+	for ok := true; ok; {
+		n, err := syscall.Read(fd, *buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%s", string((*buf)[:n]))
+		ok = (n != 0)
+	}
+}
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-	buffer := make([]byte, 1024)
+	buffer := make([]byte, BUFFER_SIZE)
+
 	for {
 		n, err := conn.Read(buffer)
 		if err != nil {
 			fmt.Println("Error reading:", err)
 			return
 		}
-		fmt.Printf("Received data: %s\n", string(buffer[:n]))
+		read_bytes(&buffer, "index.html")
+		fmt.Printf("%s\n", string(buffer[:n]))
 		body := "Hello from TCP server"
 		response := fmt.Sprintf("HTTP/1.1 200 OK\r\n"+
 			"Content-Type: text/plain\r\n"+
