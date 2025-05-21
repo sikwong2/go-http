25973851f927bffa023c0a820871a1e8c4a05fbe
Simon <simon.kwong64@gmail.com>
2025-05-21 16:21:02 -0700

    http server correctly sends the html page

--- main.go
+++ main.go
@@ -9,8 +9,7 @@ import (
 
 const BUFFER_SIZE = 4096
 
-func read_bytes(buf *[]byte, infile string) {
-	fmt.Printf("Reading %s\n", infile)
+func read_bytes(buf *[]byte, infile string) int {
 	fd, err := syscall.Open(infile, syscall.O_RDONLY, 0)
 	if err != nil {
 		log.Fatal(err)
@@ -18,34 +17,40 @@ func read_bytes(buf *[]byte, infile string) {
 
 	defer syscall.Close(fd)
 
+	r := 0
 	for ok := true; ok; {
 		n, err := syscall.Read(fd, *buf)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Printf("%s", string((*buf)[:n]))
+		r += n
+		// fmt.Printf("%s", string((*buf)[:n]))
 		ok = (n != 0)
 	}
+	return r
 }
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	buffer := make([]byte, BUFFER_SIZE)
+	headers := make([]byte, BUFFER_SIZE)
 
 	for {
-		n, err := conn.Read(buffer)
+		n, err := conn.Read(headers)
 		if err != nil {
 			fmt.Println("Error reading:", err)
 			return
 		}
-		read_bytes(&buffer, "index.html")
-		fmt.Printf("%s\n", string(buffer[:n]))
-		body := "Hello from TCP server"
+		r := read_bytes(&buffer, "index.html")
+		fmt.Printf("%s\n", string(headers[:n]))
+		// body := "Hello from TCP server"
 		response := fmt.Sprintf("HTTP/1.1 200 OK\r\n"+
-			"Content-Type: text/plain\r\n"+
+			"Content-Type: text/html\r\n"+
 			"Content-Length: %d\r\n"+
-			"\r\n%s", len(body), body)
+			"\r\n%s", r, string(buffer[:r]))
+
+		fmt.Println(response)
 		_, err = conn.Write([]byte(response))
 		if err != nil {
 			fmt.Println("Error writing:", err)
